ac232d57a60562f31937d1b9b6ef8779f0dc4683
Jane Wong <jaynellew@gmail.com>
2016-08-07 06:17:43 +0800

    Update styles for explore and menu pages

--- smartphone/js/indexmenu.js
+++ smartphone/js/indexmenu.js
@@ -1,5 +1,5 @@
 var arrayMonth = ['1.  element', '2.  document', '3.  current', '4.  accelerate', '5. Jupitar'];
-var arrayToday = ['1.  junk', '2.  baby'];
+var arrayToday = ['1.  bitcoin', '2.  cloud'];
 var arrayWeek = ['1.  conflict', '2.  display', '3.  application', '4.  clear', '5.  fun'];
 // function myFunction() {
 // 	if('')
@@ -11,15 +11,11 @@ var app = angular.module('app', [])
 		// $scope.months = arryayMonth;
 		// $scope.today = arrayToday;
 		// $scope.week = arrayWeek;
-		$scope.sort = '';
-		console.log('c');
+		$scope.sort = arrayToday;
 		$scope.arrayChange = function(sort){
-			console.log('b');
-			console.log(sort);
 			if(sort == 'month'){
 				$scope.sort = arrayMonth;
-				console.log()
-			} 
+			}
 			else if(sort == 'week'){
 				$scope.sort = arrayWeek;
 			} else {
